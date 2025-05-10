5392950fdf1c4e2c6ce44985c729fba81b186ed8
Ron Kuris <swcafe@gmail.com>
2025-05-10 16:43:03 -0700

    Add support for the temperature probe
    Not quite bulletproof yet. If the scan for the temperature sensor fails
    on startup, it won't report any temperatures, and it actually crashes
    because it expects at least one was inserted at some point.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 
 mod secrets;
 
+use std::{
+    collections::HashMap,
+    fmt::Write as _,
+    sync::{Mutex, OnceLock},
+};
+
 use anyhow::anyhow;
 use esp_idf_hal::{
     delay::FreeRtos,
@@ -15,12 +21,15 @@ use esp_idf_svc::{
     http::{self, server::EspHttpServer, Method},
     log::{set_target_level, EspLogger},
     nvs::EspDefaultNvsPartition,
-    wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
     sys,
+    wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 use log::{info, warn};
-use onewire::OneWire;
+use onewire::{ds18b20, DeviceSearch, OneWire, DS18B20};
 
+static LATEST_TEMPS: OnceLock<Mutex<HashMap<[u8; 8], f64>>> = OnceLock::new();
+
+#[allow(clippy::too_many_lines)]
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -53,34 +62,81 @@ fn main() -> anyhow::Result<()> {
     ts_driver.enable()?;
     info!("Temperature is {}F", ts_driver.get_fahrenheit()?);
 
+    info!("Setting up the temperature probe");
+    let mut pin = PinDriver::input_output_od(peripherals.pins.gpio2)?;
+    let mut wire = OneWire::new(&mut pin, false);
+    let mut delay = esp_idf_hal::delay::Delay::new(10000);
+    let mut sensors = HashMap::new();
+    if wire.reset(&mut delay).is_err() {
+        warn!("extern temperature probe reset failed");
+    } else {
+        let mut search = DeviceSearch::new();
+        while let Ok(Some(device)) = wire.search_next(&mut search, &mut delay) {
+            info!("found device {device:?}");
+            let addr = device.address;
+            if let Ok(ds18b20) = DS18B20::new(device) {
+                info!("and it was a ds18b20");
+                let resolution = ds18b20.measure_temperature(&mut wire, &mut delay).unwrap();
+                delay.delay_ms(u32::from(resolution.time_ms()));
+                let temperature = ds18b20
+                    .read_temperature(&mut wire, &mut delay)
+                    .map(ds18b20::split_temp)
+                    .map(|(integral, fractions)| {
+                        f64::from(integral) + f64::from(fractions) / 10000.0
+                    })
+                    .unwrap();
+                info!("success! Temperature: {temperature}");
+                sensors.insert(addr, ds18b20);
+                LATEST_TEMPS
+                    .get_or_init(|| Mutex::new(HashMap::new()))
+                    .lock()
+                    .unwrap()
+                    .insert(addr, temperature);
+            } else {
+                warn!("Device didn't seem to be a ds18b20");
+            }
+        }
+    }
+
     info!("Setting up a web server");
     let mut server = create_server(&mut wifi)?;
     server.fn_handler("/", Method::Get, move |req| {
-        req.into_ok_response()?
-            .write_all(
-                format!(
-                    r#"{{
+        let others = LATEST_TEMPS
+            .get()
+            .unwrap()
+            .lock()
+            .unwrap()
+            .iter()
+            .map(|(addr, &temp)| {
+                (
+                    // convert the addr to X:X:X:X:X:X:X:X
+                    addr.iter().map(u8::to_string).collect::<Vec<_>>().join(":"),
+                    // convert the temperature into farenheight
+                    temp * 9.0 / 5.0 + 32.0,
+                )
+            })
+            .fold(String::new(), |mut output, (addr, temp): (String, f64)| {
+                // leading comma is safe here because we always have an internal
+                // temperature first
+                let _ = write!(output, r#", "{addr}": {temp}"#);
+                output
+            });
+
+        req.into_ok_response()?.write_all(
+            format!(
+                r#"{{
                         "units": "farenheit",
                         "sensors": {{
-                            "internal": "{}"
+                            "internal": {}
+                            `{}
                         }}
                     }}"#,
-                    ts_driver.get_fahrenheit().unwrap()
-                )
-                .as_bytes(),
+                ts_driver.get_fahrenheit().unwrap(),
+                others,
             )
+            .as_bytes(),
+        )
     })?;
-
-    info!("Setting up the temperature probe");
-    let mut pin = PinDriver::input_output_od(peripherals.pins.gpio10)?;
-    let mut wire = OneWire::new(&mut pin, false);
-    let mut delay = esp_idf_hal::delay::Delay::new(10000);
-    if wire.reset(&mut delay).is_err() {
-        warn!(
-            "extern temperature probe reset failed on pin {:?}",
-            pin.pin()
-        );
-    }
 
     info!("flashing the user LED");
 
@@ -88,7 +144,25 @@ fn main() -> anyhow::Result<()> {
 
     loop {
         led.toggle()?;
-        FreeRtos::delay_ms(500);
+        FreeRtos::delay_ms(2000);
+        for (addr, ds18b20) in &sensors {
+            let resolution = ds18b20.measure_temperature(&mut wire, &mut delay).unwrap();
+            delay.delay_ms(u32::from(resolution.time_ms()));
+            if let Ok(temperature) = ds18b20
+                .read_temperature(&mut wire, &mut delay)
+                .map(ds18b20::split_temp)
+                .map(|(integral, fractions)| f64::from(integral) + f64::from(fractions) / 10000.0)
+            {
+                info!("success! Temperature: {temperature}");
+                LATEST_TEMPS
+                    .get_or_init(|| Mutex::new(HashMap::new()))
+                    .lock()
+                    .unwrap()
+                    .insert(*addr, temperature);
+            } else {
+                warn!("unable to read temp");
+            }
+        }
     }
 
     /*
