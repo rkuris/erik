284abfa5a2cc81b0ac77df05836020b2e05d5d53
Ron Kuris <ron.kuris@avalabs.org>
2025-06-17 09:30:39 -0700

    Fixup build script
    If secrets.rs doesn't exist, copy it from secrets.rs.example and emit a
    warning.

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
 use embuild::espidf::sysenv;
 
+use std::path::PathBuf;
+
 fn main() {
     sysenv::output();
+    let secretfile = PathBuf::from("src/secrets.rs");
+    if !secretfile.exists() {
+        println!("cargo::warning=Using secrets.rs.example, with some default secrets {}", std::env::current_dir().unwrap().display());
+        std::fs::copy("src/secrets.rs.example", secretfile).expect("copy of secrets.rs.example to secrets.rs failed");
+    }
 }
